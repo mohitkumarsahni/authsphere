19246c83503ef46245a6b43076ef824ea7db5f20
Mohit Sahni <mohitkumarsahni@gmail.com>
2025-03-01 02:16:46 +0530

    Disable flyway in test

--- src/test/java/org/sahni/AppStatusTest.java
+++ src/test/java/org/sahni/AppStatusTest.java
@@ -1,6 +1,7 @@
 package org.sahni;
 
 import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
@@ -9,6 +10,7 @@ import static org.hamcrest.CoreMatchers.is;
 @QuarkusTest
 class AppStatusTest {
     @Test
+    @DisplayName("test app status endpoint for 200")
     void testAppStatusEndpoint() {
         given()
           .when().get("/app_status")
